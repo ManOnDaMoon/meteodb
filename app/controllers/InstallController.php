c2de58eda5ee7b9d4d24aa4e06b405d7fa8ce7f0
ManOnDaMoon <manondamoon@gmail.com>
2026-01-06 13:29:11 +0100

    Install tables

--- app/controllers/InstallController.php
+++ app/controllers/InstallController.php
@@ -9,6 +9,49 @@ use app\records\UserRecord;
 
 class InstallController extends BaseController
 {
+    $sqlDataPointsTable = "
+        CREATE TABLE IF NOT EXISTS `DataPoints` (
+        	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        	`station_id` VARCHAR(30) NOT NULL ,
+        	`action` VARCHAR(30) ,
+        	`realtime` INT ,
+        	`rtfreq` INT ,
+        	`dateutc` DATETIME NOT NULL ,
+        	`baromin` FLOAT ,
+        	`tempf` FLOAT  ,
+        	`dewptf` FLOAT ,
+        	`humidity` INT ,
+        	`windspeedmph` FLOAT,
+        	`windgustmph` FLOAT,
+        	`winddir` INT ,
+        	`rainin` FLOAT ,
+        	`dailyrainin` FLOAT,
+        	`solarradiation` FLOAT,
+        	`UV` FLOAT ,
+        	`indoortempf` FLOAT ,
+        	`indoorhumidity` INT ,
+            INDEX (`station_id`, `dateutc`)
+        ) ENGINE = InnoDB;";
+    
+    $sqlStationsTable = "
+        CREATE TABLE IF NOT EXISTS `Stations` (
+            `station_id` VARCHAR(30) NOT NULL PRIMARY KEY,
+            `description` VARCHAR(256) ,
+            `position` POINT ,
+            `key` VARCHAR(60) NOT NULL,
+            `last_update` DATETIME
+        ) ENGINE = InnoDB;
+        ";
+    
+    $sqlUsersTable = "
+        CREATE TABLE IF NOT EXISTS `Users` (
+            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `username` VARCHAR(30) NOT NULL,
+            `password` VARCHAR(60) NOT NULL,
+            UNIQUE(`username`)
+        ) ENGINE = InnoDB;
+        ";
+    
     public function index(): void
     {
         $UserRecord = new UserRecord($this->db());
@@ -18,6 +61,14 @@ class InstallController extends BaseController
         if ($UserRecord->isHydrated()) {
             $result = "Installation déjà effectuée.";
         } else {
+            try {
+                $this->db()->query($sqlDataPointsTable);
+                $this->db()->query($sqlStationsTable);
+                $this->db()->query($sqlUsersTable);
+            } catch (Exception $e) {
+                $result = "Erreur lors de la création des tables : " . print_r($this->db()->errorInfo(),true);
+            }
+            
             $UserRecord->id = 1;
             $UserRecord->username = 'admin';
             $UserRecord->password = password_hash('password', PASSWORD_DEFAULT);
