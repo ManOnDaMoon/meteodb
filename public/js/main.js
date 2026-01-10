0aac61d7a1058de78188a5ca4c467c5a33525557
ManOnDaMoon <manondamoon@gmail.com>
2026-01-10 18:53:56 +0100

    Improve chart style and code

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,83 @@
-fetch('/station/AVON/daily', { 
-  method: 'GET'
-})
-.then(function(response) { return response.json(); })
-.then(function(jsonData) {
-	var chart = c3.generate({
-	    data: {
-	        x: 'Time',
-			xFormat: '%Y-%m-%d %H:%M:%S',
-	        json: jsonData,
-			keys: {
-			        x: 'hour',
-			        value: ['tempc', 'humidity']
-			      },
-			axes: {
-				tempc: 'y',
-				humidity: 'y2'
-			}
-	    },
-	    axis: {
-	        x: {
-	            type: 'timeseries',
-	            tick: {
-	                format: '%H:00'
-	            }
-	        },
-			y: {
-				
-			},
-			y2: {
-				show: true
-			}
-	    }
-	});
-});
+
+/**
+ *  Draw chart for daily data - using specific date format showin only hours
+ * 
+ *  TODO : add value series as parameter in div attribute
+ */
+function drawDailyChart(chartId, value, valueName, valueUnit, valueMin = null, valueMax = null) {
+	dailyGraph = document.getElementById(chartId);
+	if (dailyGraph != null) {
+		fetch(dailyGraph.getAttribute('data-url'), { 
+		  method: 'GET'
+		})
+		.then(function(response) { return response.json(); })
+		.then(function(jsonData) {
+			var chart = c3.generate({
+				bindto: '#' + chartId,
+			    data: {
+			        x: 'Time',
+					xFormat: '%Y-%m-%d %H:%M:%S',
+			        json: jsonData,
+					keys: {
+					        x: 'hour',
+					        value: [value]
+					      },
+					axes: {
+						value,
+					},
+					names: {
+						[value]: valueName,
+					}
+			    },
+			    axis: {
+			        x: {
+			            type: 'timeseries',
+			            tick: {
+			                format: '%H:00'
+			            }
+			        },
+					y: {
+						label: valueUnit,
+						min: valueMin,
+						max: valueMax
+					}
+			    },
+				grid: {
+					y: {
+						show: true
+					}	
+				},
+				tooltip: {
+			        format: {
+						value: function (v, id, i, j) { return v + '°C'; } // apply this format to both y and y2
+	        		}
+				}
+			});
+		});
+	};	
+};
+
+/**
+ *  Refresh button activation timeout
+ */
+function refreshButtonSetTimeout(refreshButtonId, timeout) {
+	setTimeout(
+		function() {
+			$refresh = document.getElementById(refreshButtonId);
+    		$refresh.addEventListener('click', function() { location.reload() }, false);
+    		$refresh.removeAttribute('disabled');
+		},
+		timeout
+	);
+}
+
+
+// On Document load
+document.addEventListener(
+"DOMContentLoaded",
+function() {
+	refreshButtonSetTimeout('refresh', 6000);
+	
+	drawDailyChart('daily_chart', 'tempc', 'Température', '°C');
+
+}); 
